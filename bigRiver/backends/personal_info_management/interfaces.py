e184367cc522a0e2054676445e5741a67dd3531a
yljustsk8 <1848772469@qq.com>
2018-08-30 14:29:57 +0800

    test

--- bigRiver/backends/personal_info_management/interfaces.py
+++ bigRiver/backends/personal_info_management/interfaces.py
@@ -5,20 +5,10 @@ django.setup()
 #个人信息管理模块
 
 from basic_info.models import *
-from bigRiver.view import UserForm
 
 
 def login(request):
-    uf = UserForm(request.POST())
-    if uf.is_valid():
-        # 获取表单用户密码
-        userID = uf.cleaned_data['username']
-        password = uf.cleaned_data['password']
-        model = personal_info.objects.get(userID=userID)
-        if(model.password==password):
-            print(userID + " in pim.interfaces.login success!")
-            return True
-        return False
+    return True
 
 def register():
     return True
