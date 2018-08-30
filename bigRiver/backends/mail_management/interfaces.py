fbe254e91359f40472a10b61bdb96d0451d2bf77
yljustsk8 <1848772469@qq.com>
2018-08-30 23:59:14 +0800

    新建了接口文件，暂时完成了personal_info_management中的接口
    by jyl

---
+++ bigRiver/backends/mail_management/interfaces.py
@@ -0,0 +1,8 @@
+
+
+# 1.	bool send_invitation(userID,companyID)
+# 2.	bool answer_invitation(userID,companyID ,bool)
+# 3.	bool send_request(userID,date,type)
+# 4.	List<requestID> show_requests()
+# 5.	bool handle_request(requestID)
+# 6.	< userID,companyID ,bool > fetch_request(requestID)
--- bigRiver/basic_info/models.py
+++ bigRiver/basic_info/models.py
@@ -3,17 +3,18 @@ from django.db import models
 # Create your models here.
 
 class personal_info(models.Model):
-    userID = models.CharField(max_length=10)
-    password = models.CharField(max_length=20)
-    email = models.CharField(max_length=50)
-    name = models.CharField(max_length=10)
-    company = models.CharField(max_length=20)
-    departName = models.CharField(max_length=10)
-    modelLocation = models.CharField(max_length=10)
+    userID = models.CharField(max_length=10, default='')
+    password = models.CharField(max_length=20, default='')
+    email = models.CharField(max_length=50, default='')
+    name = models.CharField(max_length=10, default='')
+    company = models.CharField(max_length=20, default='')
+    departName = models.CharField(max_length=10, default='')
+    modelLocation = models.CharField(max_length=10, default='')
 
 class company_info(models.Model):
-    companyID = models.CharField(max_length=20)
-    taxNumber = models.CharField(max_length=20)
-    bossID = models.CharField(max_length=10)
-    adminID = models.CharField(max_length=10)
-    departNames = models.CharField(max_length=10)
+    companyID = models.CharField(max_length=20, default='')
+    name = models.CharField(max_length=20, default='')
+    taxNumber = models.CharField(max_length=20, default='')
+    bossID = models.CharField(max_length=10, default='')
+    adminID = models.CharField(max_length=10, default='')
+    departNames = models.CharField(max_length=10, default='')
