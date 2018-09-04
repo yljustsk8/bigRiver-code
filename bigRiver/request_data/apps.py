e5c7522a5a99c5c5d9a57480ff9620e5b87a458a
yljustsk8 <1848772469@qq.com>
2018-09-04 11:18:47 +0800

    更新了模型

---
+++ bigRiver/request_data/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class RequestDataConfig(AppConfig):
+    name = 'request_data'
---
+++ bigRiver/request_data/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+#申请加入   type=1  senderID = userID  receiverID = companyID
+#邀请加入   type=2  senderID = companyID  receiverID = userID
+#申请请假   type=3  senderID = userID  receiverID = companyID
+#申请补卡   type=4  senderID = userID  receiverID = companyID
+
+# Create your models here.
+class requests(models.Model):
+    requestID = models.CharField(max_length=10, default='')
+    senderID = models.CharField(max_length=10, default='')
+    receiverID = models.CharField(max_length=10, default='')
+    date = models.CharField(max_length=10, default='')
+    type = models.IntegerField(default=-1)
+    content = models.CharField(max_length=100, default='')
+    dealed = models.BooleanField(default=False)
+    result = models.IntegerField(default=-1)
